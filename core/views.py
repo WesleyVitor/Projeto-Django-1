045c6715e11c1631c37e31e39261fe7b9eba62bc
WesleyVitor <wesleydemorais@outlook.com.br>
2021-09-17 20:14:32 -0300

    Criando o primeiro formulario usando a classe Form do django

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from .forms import ContatoForm
 # Create your views here.
 
 
@@ -8,7 +9,31 @@ def index(request):
 
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print("Mensagem Enviada!")
+            print(f"Nome:{nome}")
+            print(f"Email:{email}")
+            print(f"Assunto:{assunto}")
+            print(f"Mensagem:{mensagem}")
+
+            messages.success(request, 'Email Enviado com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar o email!')
+
+
+    context ={
+        'form': form
+    }
+    return render(request, 'contato.html', context)
 
 
 def produto(request):
