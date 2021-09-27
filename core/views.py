dc141804863337d0553b4fdeb92297263e1d265b
WesleyVitor <wesleydemorais@outlook.com.br>
2021-09-27 11:12:54 -0300

    Criando formulario com modelForm

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib import messages
-from .forms import ContatoForm
+from .forms import ContatoForm, ProdutoModelForm
 # Create your views here.
 
 
@@ -27,5 +27,25 @@ def contato(request):
 
 
 def produto(request):
-    return render(request, 'produto.html')
+    if str(request.method) == 'POST':
+        form = ProdutoModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            prod = form.save(commit=False)
+
+            print(f"Nome:{prod.nome}")
+            print(f"Preço:{prod.preco}")
+            print(f"Estoque:{prod.estoque}")
+            print(f"Imagem:{prod.imagem}")
+
+            messages.success(request, "Produto salvo com sucesso.")
+            form = ProdutoModelForm()
+        else:
+            messages.error(request, "Erro ao salvar Produto.")
+    else:
+        form = ProdutoModelForm()
+    
+    context = {
+        "form":form
+    }
+    return render(request, 'produto.html', context)
 
