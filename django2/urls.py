6568f324ebf9af7650858598e6e8e53fa98533ec
WesleyVitor <wesleydemorais@outlook.com.br>
2021-09-27 13:39:59 -0300

    Salvando dados no banco e configurando medias

--- django2/urls.py
+++ django2/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+## Permite que os templates possam utilizar as medias adicionadas
+##+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
