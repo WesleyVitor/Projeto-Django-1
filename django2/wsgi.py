d4d3849f964e73c0657aaa3d519c9aa8a1907a5b
WesleyVitor <wesleydemorais@outlook.com.br>
2021-09-30 13:41:04 -0300

    Usando dj_static

--- django2/wsgi.py
+++ django2/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 
+#Cling -> suporte aos arquivos estaticos
+#MediaCling -> suporte às medias do usuario
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django2.settings')
 
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
