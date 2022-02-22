04c320e1bc1e409a1c0b0818cf48126e80ea625c
WesleyVitor <wesleydemorais@outlook.com.br>
2022-02-22 19:39:47 -0300

    fix: db change

--- django2/settings.py
+++ django2/settings.py
@@ -83,18 +83,14 @@ WSGI_APPLICATION = 'django2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django2',
-        'USER': 'mysql',
-        'PASSWORD': 'mysql123',
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
